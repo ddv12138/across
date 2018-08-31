a3122098c371e85e8d78f7b5cf0716c61b2c8d02
ddv12138 <644077730@qq.com>
2018-08-31 03:34:53 +0000

    删除文件

--- backup-seafile-here.sh
+++
@@ -1,1 +0,0 @@
-rsync -vzrtopg --progress -e ssh --delete root@pineapple.cf:/opt/seafile-data/* /seafile-data/
--- git_this.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.email "644077730@qq.com"
-git config --global user.name "ddv12138"
-git add . && git commit -m 'date' && git push origin master
